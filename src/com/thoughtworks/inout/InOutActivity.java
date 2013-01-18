100f542ea77efca40e4f3ea847ab37ff520a6b82
Fernando Junior <fernandoasfj@gmail.com>
2013-01-18 15:34:32 -0300

    Fernando - Added confirmation message/Out button

--- src/com/thoughtworks/inout/InOutActivity.java
+++ src/com/thoughtworks/inout/InOutActivity.java
@@ -4,18 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
-import com.thoughtworks.inout.db.TimeCardDAO;
+import com.thoughtworks.inout.db.SQLLiteTimeCardDAO;
 
 public class InOutActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_out);
+        buildButtons();
     }
 
-    @Override
+    private void buildButtons() {
+    	Button buttonIn = (Button) findViewById(R.id.button_in);
+    	Button buttonOut = (Button) findViewById(R.id.button_out);
+    	
+    	buttonIn.setOnClickListener(new RegisterTimeListener(this));
+    	buttonOut.setOnClickListener(new RegisterTimeListener(this));
+    }
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_in_out, menu);
@@ -23,8 +32,11 @@ public class InOutActivity extends Activity {
     }
     
     public void registerTime(View view) {
-    	TimeCard timeCard = new TimeCard(new TimeCardDAO(this));
-    	TextView console = (TextView) findViewById(R.id.console);
-    	console.setText(timeCard.punch(PunchType.IN).toString());
+    	TimeCard timeCard = new TimeCard(new SQLLiteTimeCardDAO(this));
+    	PunchType punchType = view.getId() == R.id.button_in ? PunchType.IN : PunchType.OUT;
+    	timeCard.punch(punchType);
     }
+    
+   
+    
 }
