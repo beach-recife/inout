bd3be66bf5a4439a667e33eeeae13b0f5113edcd
Fernando Junior <fernandoasfj@gmail.com>
2013-01-23 11:27:46 -0300

    Confirmation message/Buttons now with colors

--- src/com/thoughtworks/inout/InOutActivity.java
+++ src/com/thoughtworks/inout/InOutActivity.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.inout;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -8,35 +9,34 @@ import android.widget.Button;
 
 import com.thoughtworks.inout.db.SQLLiteTimeCardDAO;
 
-public class InOutActivity extends Activity {
+public class InOutActivity extends Activity implements Confirmable{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_out);
-        buildButtons();
+        addListeners();
     }
 
-    private void buildButtons() {
+    private void addListeners() {
     	Button buttonIn = (Button) findViewById(R.id.button_in);
     	Button buttonOut = (Button) findViewById(R.id.button_out);
     	
-    	buttonIn.setOnClickListener(new RegisterTimeListener(this));
-    	buttonOut.setOnClickListener(new RegisterTimeListener(this));
+    	buttonIn.setOnClickListener(new RegisterTimeListener(this, this));
+    	buttonOut.setOnClickListener(new RegisterTimeListener(this, this));
+    	buttonOut.setBackgroundColor(Color.CYAN);
     }
 
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_in_out, menu);
         return true;
     }
     
-    public void registerTime(View view) {
-    	TimeCard timeCard = new TimeCard(new SQLLiteTimeCardDAO(this));
+	public void onConfirm(View view) {
+		TimeCard timeCard = new TimeCard(new SQLLiteTimeCardDAO(this));
     	PunchType punchType = view.getId() == R.id.button_in ? PunchType.IN : PunchType.OUT;
     	timeCard.punch(punchType);
-    }
+	}
     
-   
     
 }
