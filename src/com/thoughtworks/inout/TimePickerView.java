52c7022c1fa14b60c0bc414f34ac8f039302cc80
unknown <ccrispim@BRCCRISPIM.corporate.thoughtworks.com>
2013-02-06 14:11:36 -0300

    Adding features related to date and time picker tests (WIP)

---
+++ src/com/thoughtworks/inout/TimePickerView.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.inout;
+
+import android.content.Context;
+import android.widget.ListView;
+
+public class TimePickerView extends ListView {
+
+	public TimePickerView(Context context) {
+		super(context);
+	}
+
+}
